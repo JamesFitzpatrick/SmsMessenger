1317326f3a32f002b31958d5c50cf1b78982dcb9
James Fitzpatrick <james_fitzpatrick@outlook.com>
2013-10-15 20:24:45 +0100

    Add support for "special" characters

--- SMSMessenger/SMSMessenger.vb
+++ SMSMessenger/SMSMessenger.vb
@@ -13,6 +13,11 @@
         txtMessage.Text &= c
     End Sub
 
+    Private Sub buttonSpecialClick(sender As Object, e As EventArgs) Handles btnSpec1.Click, btnSpec2.Click, btnSpec3.Click, btnSpec4.Click, btnSpec5.Click, btnSpec6.Click, btnSpec7.Click, btnSpec9.Click, btnSpec10.Click, btnSpec11.Click, btnSpec12.Click, btnSpec14.Click
+        Dim c As Char = sender.Text
+        txtMessage.Text &= c
+    End Sub
+
     Private Sub btnBackspaceClick(sender As Button, e As EventArgs) Handles btnBckSpace.Click
         If (txtMessage.Text.Length > 0) Then
             txtMessage.Text = txtMessage.Text.Substring(0, txtMessage.Text.Length - 1)
@@ -28,8 +33,9 @@
         txtMessage.Clear()
     End Sub
 
+
     Private Sub cmdClose_Click(sender As Object, e As EventArgs) Handles cmdClose.Click
-        End
+        Close()
     End Sub
 
     Private Sub Bar1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Bar1.MouseDown
